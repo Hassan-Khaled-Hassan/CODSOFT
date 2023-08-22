9df60a4f0d353705da9767f8bbb42f70ccf9bce0
hassan khaled <119671650+Hassan-Khaled-Hassan@users.noreply.github.com>
2023-08-22 16:40:10 +0300

    Add files via upload

---
+++ level1/task3/main.js
@@ -0,0 +1,58 @@
+
+// const display = document.querySelector(".display");
+// const buttons = document.querySelectorAll("button");
+// const specialChars = ["%", "*", "/", "-", "+", "="];
+// let output = "";
+
+// //Define function to calculate based on button clicked.
+// const calculate = (btnValue) => {
+//   display.focus();
+//   if (btnValue === "=" && output !== "") {
+//     //If output has '%', replace with '/100' before evaluating.
+//     output = eval(output.replace("%", "/100"));
+//   } else if (btnValue === "AC") {
+//     output = "";
+//   } else if (btnValue === "DEL") {
+//     //If DEL button is clicked, remove the last character from the output.
+//     output = output.toString().slice(0, -1);
+//   } else {
+//     //If output is empty and button is specialChars then return
+//     if (output === "" && specialChars.includes(btnValue)) return;
+//     output += btnValue;
+//   }
+//   display.value = output;
+// };
+
+// //Add event listener to buttons, call calculate() on click.
+// buttons.forEach((button) => {
+//   //Button click listener calls calculate() with dataset value as argument.
+//   button.addEventListener("click", (e) => calculate(e.target.dataset.value));
+// });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const display = document.querySelector(".display");
+  const buttons = document.querySelectorAll("button");
+  let currentInput = "";
+
+  buttons.forEach((button) => {
+    button.addEventListener("click", function () {
+      const buttonValue = this.textContent;
+
+      if (buttonValue === "=") {
+        try {
+          currentInput = eval(currentInput).toString();
+        } catch (error) {
+          currentInput = "Error";
+        }
+      } else if (buttonValue === "AC") {
+        currentInput = "";
+      } else if (buttonValue === "DEL") {
+        currentInput = currentInput.slice(0, -1);
+      } else {
+        currentInput += buttonValue;
+      }
+
+      display.value = currentInput;
+    });
+  });
+});
